f7b89fb5cfb4870fc8a6d9e6787296fe9536133a
bramgrooten <bram.grooten@gmail.com>
2020-03-10 11:03:30 +0100

    add check_left

--- serpentine/my_agent.py
+++ serpentine/my_agent.py
@@ -1,6 +1,9 @@
+import numpy as np
 from pommerman import characters
-from pommerman.constants import Action
+from pommerman.constants import Action, Item
 from pommerman.agents import BaseAgent
+
+
 
 
 class MyAgent(BaseAgent):
@@ -13,8 +16,17 @@ class MyAgent(BaseAgent):
     def act(self, obs, action_space):
         # Main event that is being called on every turn.
         if not self.queue:      # meaning, if the queue is empty
+            my_location = obs['position']
+            board = obs['board']
             self.queue.append(Action.Right)
             self.queue.append(Action.Down)
-            self.queue.append(Action.Left)
+            if self.check_left(board, my_location):
+                self.queue.append(Action.Left)
             self.queue.append(Action.Up)
         return self.queue.pop()
+
+    def check_left(self, board, my_location):
+        row, col = my_location
+        if board[row, max(col-1, 0)] == Item.Passage.value:
+            return True
+        return False
