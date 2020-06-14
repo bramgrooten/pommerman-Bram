351d9708afa91dddd9e64aae1e5d6eefa979f42b
bramgrooten <bram.grooten@gmail.com>
2020-06-14 15:43:51 +0200

    finished lesson 5

--- serpentine/my_agent.py
+++ serpentine/my_agent.py
@@ -40,7 +40,8 @@ class MyAgent(BaseAgent):
         if not self.queue:
             my_location = obs['position']
             board = obs['board']
-            goal_location = (2, 2)
+
+            goal_location = self.move_to_safe_place(obs)
 
             if self.can_place_bomb(obs['bomb_life'], obs['ammo'], my_location):
                 self.queue.append(Action.Bomb)
@@ -122,6 +123,88 @@ class MyAgent(BaseAgent):
         if not ammo > 0:
             return False
         return True
+
+    def create_danger_map(self, obs: dict) -> np.ndarray:
+        # Set our initial danger map
+        danger_map = obs['flame_life']
+
+        # Find all bomb locations, bomb timers and strength
+        bombs = np.where(obs['bomb_life'] > 0)
+        bombs_timers = map(int, obs['bomb_life'][bombs])
+        bomb_strength = map(int, obs['bomb_blast_strength'][bombs])
+
+        # Now we are going to set the danger information
+        for row, col, timer, strength in zip(*bombs, bombs_timers, bomb_strength):
+
+            # Reduce strength by one, since we are creating a `+` form, with the bomb as center.
+            strength -= 1
+
+            # Calculate the upper and lower ranges of the bombs (this is the + sign).
+            row_low, row_high = max(row - strength, 0), min(row + strength, 10)
+            col_low, col_high = max(col - strength, 0), min(col + strength, 10)
+
+            # Set the information on the danger map, first row and then column.
+            for row_danger in range(row_low, row_high + 1):
+                danger_map[row_danger, col] = timer
+
+            for col_danger in range(col_low, col_high + 1):
+                danger_map[row, col_danger] = timer
+
+        return danger_map
+
+    def find_reachable_free_space(self, board: np.ndarray, danger_map: np.ndarray, location: tuple) -> tuple:
+        """ Returns the location of a safe space that can be reached from the current location.  """
+        to_visit = [location]
+        visited = []
+        safest_place = location
+
+        while to_visit:
+            row, col = to_visit.pop(0)
+
+            for direction in Directions.ALL:
+                new_point = direction.array + np.array([row, col])
+
+                # Check if position is on the board (2nd time we use this, refactor?)
+                if min(new_point) < 0 or max(new_point) > 10:
+                    continue
+
+                # This makes checking the numpy positions easier
+                new_point = tuple(new_point)
+
+                # here we make the two checks, is it a passage, is it dangerous
+                passage = board[new_point] == Item.Passage.value
+                danger = danger_map[new_point] > 0
+
+                # If we can move there and there is no danger we found a safe haven
+                if passage and not danger:
+                    return new_point
+
+                # If we can move there but it is dangerous, check if it is safer than where we are now.
+                if passage and danger:
+                    if danger_map[safest_place] > danger_map[new_point]:
+                        safest_place = new_point
+
+                # Add the new point if we haven't been there before.
+                if passage and new_point not in visited:
+                    to_visit.append(new_point)
+
+            visited.append((row, col))
+        return location
+
+    def move_to_safe_place(self, obs: dict) -> tuple:
+        """ Returns a location to which we can safely move.  """
+
+        # Find all position that can lead to our destruction.
+        danger_map = self.create_danger_map(obs)
+
+        # Check if our current position is safe, if so we can go/stay there.
+        my_location = obs['position']
+        if danger_map[my_location] == 0:
+            return my_location
+
+        # Find a reasonable safer location
+        safe_position = self.find_reachable_free_space(obs['board'], danger_map, my_location)
+        return safe_position
 
 
 
